b7921720bb7c27fa1d9a4294961ddf5c32cd8b1b
Taibu Haroub <haroubtaibu@gmail.com>
2022-02-23 15:23:22 +0300

    Add all posts page

--- components/posts/AllPosts.js
+++ components/posts/AllPosts.js
@@ -1,7 +1,13 @@
-import React from "react";
+import PostsGrid from "./PostsGrid";
+import classes from "./AllPosts.module.css";
 
-function AllPosts() {
-  return <div>AllPosts</div>;
+function AllPosts({ posts }) {
+  return (
+    <section className={classes.posts}>
+      <h1>All Posts</h1>
+      <PostsGrid posts={posts} />
+    </section>
+  );
 }
 
 export default AllPosts;
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,7 +1,38 @@
-import React from "react";
+import AllPosts from "../../components/posts/AllPosts";
 
-function AllPosts() {
-  return <div>index</div>;
+const DUMMY_POSTS = [
+  {
+    title: "Gee=tting Started with NextJs",
+    image: "getting-started-nextjs.png",
+    excerpt: "NextJs is the react framework for the future",
+    date: "2022-02-10",
+    slug: "getting-started-with-nextjs1",
+  },
+  {
+    title: "Gee=tting Started with NextJs",
+    image: "getting-started-nextjs.png",
+    excerpt: "NextJs is the react framework for the future",
+    date: "2022-02-10",
+    slug: "getting-started-with-nextjs2",
+  },
+  {
+    title: "Gee=tting Started with NextJs",
+    image: "getting-started-nextjs.png",
+    excerpt: "NextJs is the react framework for the future",
+    date: "2022-02-10",
+    slug: "getting-started-with-nextjs3",
+  },
+  {
+    title: "Gee=tting Started with NextJs4",
+    image: "getting-started-nextjs.png",
+    excerpt: "NextJs is the react framework for the future",
+    date: "2022-02-10",
+    slug: "getting-started-with-nextjs4",
+  },
+];
+
+function AllPostsPage() {
+  return <AllPosts posts={DUMMY_POSTS} />;
 }
 
-export default AllPosts;
+export default AllPostsPage;
