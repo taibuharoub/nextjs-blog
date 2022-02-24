ac969ddf3053a50fbffc76c7d0c9b9a842012cce
Taibu Haroub <haroubtaibu@gmail.com>
2022-02-24 14:29:55 +0300

    Render Render code snippets from markdown

--- components/posts/post-detail/PostContent.js
+++ components/posts/post-detail/PostContent.js
@@ -1,5 +1,11 @@
 import Image from "next/image";
 import ReactMarkdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import {
+  atomDark,
+  darcula,
+  dark,
+} from "react-syntax-highlighter/dist/cjs/styles/prism"; //for styling
 
 import classes from "./PostContent.module.css";
 import PostHeader from "./PostHeader";
@@ -38,6 +44,39 @@ function PostContent({ post }) {
       }
       return <p>{paragraph.children}</p>;
     }, // can use the above custom renderer or this one
+
+    code(code) {
+      // const { language, value } = code;
+      const { className, children } = code;
+      // console.log(code);
+      const language = className.split("-")[1]; // className is something like language-js => We need the "js" part here
+      const match = /language-(\w+)/.exec(className || "");
+      // console.log(match);
+      return (
+        <SyntaxHighlighter
+          style={atomDark}
+          language={match[1]}
+          // language={language}
+          children={children}
+        />
+      );
+    },
+    // code({ node, inline, className, children, ...props }) {
+    //   const match = /language-(\w+)/.exec(className || "");
+    //   return !inline && match ? (
+    //     <SyntaxHighlighter
+    //       children={String(children).replace(/\n$/, "")}
+    //       style={dark}
+    //       language={match[1]}
+    //       PreTag="div"
+    //       {...props}
+    //     />
+    //   ) : (
+    //     <code className={className} {...props}>
+    //       {children}
+    //     </code>
+    //   );
+    // },
   };
 
   return (
